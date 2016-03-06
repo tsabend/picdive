577d7ea4117513fb09d2a9c19281fb0349c87750
seane235 <seane235@gmail.com>
2016-03-06 15:09:48 -0500

    GifViewController inherits from PicDiveModalViewController

--- picdive/GifViewController.swift
+++ picdive/GifViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class GifViewController: UIViewController {
+
+class GifViewController: PicDiveModalViewController {
     
     var gif: UIImage? {
         didSet {
@@ -17,34 +18,15 @@ class GifViewController: UIViewController {
     }
     
     private let gifView = UIImageView()
-    private let shareButton = UIButton()
-    private let backButton = UIButton()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = UIColor.PDOrange()
-        
-        self.backButton.setTitle("↑", forState: .Normal)
-        self.backButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        self.backButton.addTarget(self, action: "dismiss", forControlEvents: .TouchUpInside)
-        self.backButton.backgroundColor = UIColor.PDPurple()
-        
-        self.shareButton.setTitle("Share", forState: .Normal)
-        self.shareButton.setTitleColor(UIColor.PDPurple(), forState: .Normal)
-        self.shareButton.addTarget(self, action: "share", forControlEvents: .TouchUpInside)
-        
+    
         self.view.addSubview(self.gifView)
-        self.view.addSubview(self.shareButton)
-        self.view.addSubview(self.backButton)
         
     }
     
-    func dismiss() {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func share() {
+    override func share() {
         if let image = self.gif {
             let shareItems: Array = [image]
             let activityViewController: UIActivityViewController = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
@@ -55,15 +37,11 @@ class GifViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
-        self.backButton.width = self.view.width
-        self.backButton.height = 60
-        
+
         self.gifView.width = self.view.width
         self.gifView.height = self.view.width
         self.gifView.moveBelow(siblingView: self.backButton, margin: 0)
         
-        self.shareButton.sizeToFit()
         self.shareButton.moveBelow(siblingView: self.gifView, margin: 110, alignment: .Center)
         
     }
---
+++ picdive/PicDiveModalViewController.swift
@@ -0,0 +1,55 @@
+//
+//  PicDiveModalViewController.swift
+//  picdive
+//
+//  Created by Sean on 3/6/16.
+//  Copyright © 2016 Sean. All rights reserved.
+//
+
+import UIKit
+
+class PicDiveModalViewController: UIViewController {
+
+    let shareButton = UIButton()
+    let backButton = UIButton()
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.PDOrange()
+        
+        self.backButton.setTitle("↑", forState: .Normal)
+    
+        self.backButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        self.backButton.addTarget(self, action: "dismiss", forControlEvents: .TouchUpInside)
+        self.backButton.backgroundColor = UIColor.PDPurple()
+        
+        self.shareButton.setTitle("Share", forState: .Normal)
+        self.shareButton.setTitleColor(UIColor.PDPurple(), forState: .Normal)
+        self.shareButton.addTarget(self, action: "share", forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(self.shareButton)
+        self.view.addSubview(self.backButton)
+        
+    }
+    
+    func share() {
+        // NO OP
+        // implement in subclass
+    }
+    
+    func dismiss() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.backButton.width = self.view.width
+        self.backButton.height = 60
+        
+        self.shareButton.sizeToFit()
+
+    }
+
+    
+}
