49fc85e8e19dd887ae8d39b40e3cdd433622f158
tsabend <tsabend@gmail.com>
2016-05-01 11:00:17 -0400

    Generate the innermost box
    This simplifies the scope interface

--- picdive/PicScopeView.swift
+++ picdive/PicScopeView.swift
@@ -15,29 +15,25 @@ class PicScopeView: UIView {
     
     /// The frame of the inner box. Setting this value
     /// will regenerate the boxes
-    var innerRect: CGRect {
-        get {
-            return self.innerBox.frame
-        }
-        set {
-            self.innerBox.frame = newValue
+    var innerRect: CGRect = CGRect.zero {
+        didSet {
             self.resetBoxes()
         }
     }
 
     /// The frames of all of the boxes
-    var frames: [CGRect] {
-        let boxes = self.boxes + [self.innerBox]
-        return boxes.map{$0.frame}
-    }
+    var frames: [CGRect] { return self.boxes.map{$0.frame} }
     
     private var boxes: [UIView] = []
-    private let innerBox = BoxView(frame: CGRect.zero)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.setupBoxGestures()
-        self.addSubview(self.innerBox)
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(PicScopeView.boxWasMoved))
+        self.addGestureRecognizer(pan)
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(PicScopeView.boxWasPinched))
+        self.addGestureRecognizer(pinch)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -48,33 +44,23 @@ class PicScopeView: UIView {
     private func resetBoxes() {
         self.boxes.forEach {$0.removeFromSuperview() }
         self.boxes = self.buildRects().map { BoxView(frame: $0) }
+        self.boxes.last?.backgroundColor = UIColor.PDLightGray().colorWithAlphaComponent(0.33)
         self.boxes.forEach(self.addSubview)
     }
     
     private func buildRects() -> [CGRect] {
-        return Array(self.frame.squaresBetween(rect: self.innerRect, steps: self.numberOfSteps).dropLast())
+        return self.frame.squaresBetween(rect: self.innerRect, steps: self.numberOfSteps)
     }
     
     // MARK: - Gestures
-    private func setupBoxGestures() {
-        self.innerBox.backgroundColor = UIColor.grayColor().colorWithAlphaComponent(0.4)
-        self.innerBox.userInteractionEnabled = true
-        
-        let pan = UIPanGestureRecognizer(target: self, action: #selector(PicScopeView.boxWasMoved))
-        self.addGestureRecognizer(pan)
-        
-        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(PicScopeView.boxWasPinched))
-        self.addGestureRecognizer(pinch)
-    }
-    
     func boxWasMoved(pan: UIPanGestureRecognizer) {
-        let view = self.innerBox
+        let innerRect = self.innerRect
 
         if pan.state == .Changed {
             
-            let viewSize = view.size
+            let viewSize = innerRect.size
             let translation = pan.translationInView(self)
-            var center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+            var center = CGPoint(x: innerRect.center.x + translation.x, y: innerRect.center.y + translation.y)
             var resetTranslation = CGPoint(x: translation.x, y: translation.y)
             if center.x - viewSize.width / 2 < 0 {
                 center.x = viewSize.width / 2
@@ -91,28 +77,24 @@ class PicScopeView: UIView {
             } else {
                 resetTranslation.y = 0
             }
-            view.center = center
+            self.innerRect.center = center
             pan.setTranslation(CGPoint.zero, inView: self)
             self.resetBoxes()
-        }
-        if pan.state == .Ended {
-            
-            self.innerRect = view.frame
         }
     }
     
     
     func boxWasPinched(pinch: UIPinchGestureRecognizer) {
-        let view = self.innerBox
+        let innerRect = self.innerRect
         if pinch.state == .Began {
             self.boxes.forEach {$0.removeFromSuperview() }
         }
-        let newSideLength = view.width * pinch.scale
+        let newSideLength = innerRect.width * pinch.scale
 
-        if view.origin.x + newSideLength < self.width && view.origin.y + newSideLength < self.height {
-            let center = view.center
-            view.size = CGSize(width: newSideLength, height: newSideLength)
-            view.center = center
+        if innerRect.origin.x + newSideLength < self.width && innerRect.origin.y + newSideLength < self.height {
+            let center = innerRect.center
+            self.innerRect.size = CGSize(width: newSideLength, height: newSideLength)
+            self.innerRect.center = center
         }
         pinch.scale = 1
         if pinch.state == .Ended {
